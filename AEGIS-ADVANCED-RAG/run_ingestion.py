4f78b351336c42f60003faf55897a9aa0c62d553
sudheer <saisudheerkonduri1@gmail.com>
2026-05-27 11:31:00 +0530

    Tested code with updated api key

--- AEGIS-ADVANCED-RAG/run_ingestion.py
+++ AEGIS-ADVANCED-RAG/run_ingestion.py
@@ -1,4 +1,6 @@
 from dotenv import load_dotenv
+load_dotenv()
+
 from langchain_text_splitters import RecursiveCharacterTextSplitter
 from langchain_text_splitters import MarkdownTextSplitter, MarkdownHeaderTextSplitter
 import os
@@ -14,7 +16,7 @@ from weaviate.classes.init import Auth
 
 chunk_size = 500
 chunk_overlap = int(chunk_size*0.13)    
-load_dotenv()
+
 
 llm = ChatOpenAI(model="gpt-4o-mini", 
                  api_key=os.getenv("OPENAI_API_KEY")
@@ -34,7 +36,7 @@ def process_files(src_files: str):
                 with open(file_path, "r", encoding="utf-8") as f:
                     text = f.read()
                     chunks = retrieve_chunks(text, chunk_size=chunk_size, chunk_overlap=chunk_overlap)
-                    metadata_chunks = metadata_tagging(chunks)
+                    metadata_chunks = metadata_tagging(chunks, llm)
                     #pprint(metadata[:5])
                     upsert_chunks_batch(metadata_chunks, wv_client, 50)
                     print("Ingestion Completed for file: ", file_path)
